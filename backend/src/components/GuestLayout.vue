868e63f7f255d37462f0866b2a80a082e3c5697a
jubi20000905 <66117384+jubi09@users.noreply.github.com>
2025-09-27 19:05:55 +0700

    update template

---
+++ backend/src/components/GuestLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img class="mx-auto h-10 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+      <h2 class="mt-10 text-center text-2xl font-bold tracking-tight text-white">
+        {{ title }}
+      </h2>
+    </div>
+
+    <!-- Konten Slot: Form akan ditampilkan di sini -->
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <slot></slot>
+    </div>
+
+    <p class="mt-10 text-center text-sm text-gray-400">
+      Not a member?
+      <a href="#" class="font-semibold text-indigo-400 hover:text-indigo-300">Start a 14 day free trial</a>
+    </p>
+  </div>
+</template>
+
+<script setup>
+const {title} = defineProps({
+    title: String
+});
+</script>
+
+<style scoped>
+
+</style>
--- backend/src/views/Login.vue
+++ backend/src/views/Login.vue
@@ -1,12 +1,6 @@
 <template>
-  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img class="mx-auto h-10 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
-      <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
-    </div>
-
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form class="space-y-6" action="#" method="POST">
+    <GuestLayout title="Sign in to your account">
+        <form class="space-y-6" action="#" method="POST">
         <div>
           <label for="email" class="block text-sm/6 font-medium text-gray-100">Email address</label>
           <div class="mt-2">
@@ -30,20 +24,18 @@
           <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Sign in</button>
         </div>
       </form>
-
-      <p class="mt-10 text-center text-sm/6 text-gray-400">
-        Not a member?
-        {{ ' ' }}
-        <a href="#" class="font-semibold text-indigo-400 hover:text-indigo-300">Start a 14 day free trial</a>
-      </p>
-    </div>
-  </div>
+    </GuestLayout>
 </template>
 
 
 <script>
+import GuestLayout from '../components/GuestLayout.vue';
+
 export default {
-    name: "Login"
+    name: "Login",
+    components: {
+        GuestLayout,
+    }
 }
 </script>
 
