fe8807cc8acb36414f9c7eb9d019648a89d87f54
jubi20000905 <66117384+jubi09@users.noreply.github.com>
2025-09-27 15:41:21 +0700

    template

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
+import RequestPassword from "../views/Request-password.vue";
 
 const routes = [
     {
@@ -12,6 +13,11 @@ const routes = [
         path: "/login",
         name: "login",
         component: Login,
+    },
+    {
+        path: "/request-password",
+        name: "request-password",
+        component: RequestPassword,
     },
 ];
 
--- backend/src/views/Request-password.vue
+++ backend/src/views/Request-password.vue
@@ -2,7 +2,7 @@
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <img class="mx-auto h-10 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
-      <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
+      <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Request New Password</h2>
     </div>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -12,22 +12,14 @@
           <div class="mt-2">
             <input type="email" name="email" id="email" autocomplete="email" required="" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
           </div>
-        </div>
-
-        <div>
-          <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm/6 font-medium text-gray-100">Password</label>
-            <div class="text-sm">
-              <router-link to="/request-password" class="font-semibold text-indigo-400 hover:text-indigo-300">Forgot password?</router-link>
+          <div class="text-sm">
+              <a href="#" class="font-semibold text-indigo-400 hover:text-indigo-300">Remember your password?</a>
             </div>
-          </div>
-          <div class="mt-2">
-            <input type="password" name="password" id="password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
-          </div>
         </div>
 
+
         <div>
-          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Sign in</button>
+          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Submit</button>
         </div>
       </form>
 
@@ -43,7 +35,7 @@
 
 <script>
 export default {
-    name: "Login"
+    name: "Request-password"
 }
 </script>
 
